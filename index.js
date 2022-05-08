20a19ea4ee68867abaec98988288827fb46c7b74
Tadpole87 <bailey_mercer@hotmail.co.uk>
2022-05-09 00:24:22 +0100

    Added a couple more practise fetch requests
    Added requests for getting all the tracks in a season and also the result of a previous race

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ async function get1() {
         document.getElementById("main").appendChild(para);
     }
 }
+
+async function get2() {
+    const response = await fetch('http://ergast.com/api/f1/current.json'); 
+
+    var data = await response.json();
+
+    const racesArray = data.MRData.RaceTable.Races;
+    console.log(data.MRData.RaceTable.Races);
+    for (let i=0; i< racesArray.length; i++) {
+        const para = document.createElement("p");
+        para.innerHTML = racesArray[i].raceName;
+        document.getElementById("main").appendChild(para);
+    }
+}
+
+async function get3() {
+    const response = await fetch('http://ergast.com/api/f1/2022/4/results.json'); 
+
+    var data = await response.json();
+
+    console.log(data);
+     
+}
