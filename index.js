a34bdbaea58e691acfef58a03ba850a54060a4bd
Tadpole87 <bailey_mercer@hotmail.co.uk>
2022-05-10 00:08:17 +0100

    Added track selection + started gathering data
    Added:
    - Track selection box
    - Populated with fetch data
    - Created drivers array
    - Began adding useful info for each driver into the array

--- index.js
+++ index.js
@@ -31,30 +31,6 @@ function getConstructor (constructor) {
 }
 
 
-async function getCurrentStandings () {
-    constructStand = [];
-    driverStand = [];
-    const response = await fetch('http://ergast.com/api/f1/current/constructorStandings.json');
-    const response1 = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
-
-    var construct = await response.json();
-    var drivers = await response1.json();
-
-    var constructorStandings = construct.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-    var driverStandings = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-
-    for (let i=0; i < constructorStandings.length; i++) {
-        constructStand.push(constructorStandings[i].Constructor);
-    }
-
-    for (let i=0; i < driverStandings.length; i++) {
-        if (driverStandings[i].Driver.code !== "HUL") {
-            driverStand.push(driverStandings[i].Driver);
-        }
-    }
-
-}
-
 async function loadTracks () {
 
     const response = await fetch ('http://ergast.com/api/f1/2022/circuits.json');
@@ -63,7 +39,6 @@ async function loadTracks () {
 
     const circuits = data.MRData.CircuitTable.Circuits;
 
-    console.log(circuits);
     for (let i=0; i < circuits.length; i++) {
 
         const circuit = circuits[i].circuitName;
@@ -74,3 +49,86 @@ async function loadTracks () {
         document.getElementById("trackOption").appendChild(item);
     }
 }
+
+
+async function getDriverStandings (driversList) {
+    driverStand = [];
+    const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+
+    var drivers = await response.json();
+
+    var driverStandings = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+
+    for (let i=0; i < driverStandings.length; i++) {
+        for (let x=0; x < driversList.length; x++) {
+            if (driversList[x][0] === driverStandings[i].Driver.driverId) {
+                driversList[x].push(driverStandings[i].position) 
+                driversList[x].push(driverStandings[i].points);
+                driversList[x].push(driverStandings[i].Constructors[0].constructorId);
+            }
+        }
+    }
+    return driverList;
+
+}
+
+async function getConstructorStandings (driversList) {
+    constructStand = [];
+
+    const response = await fetch('http://ergast.com/api/f1/current/constructorStandings.json');
+
+    var construct = await response.json();
+
+    var constructorStandings = construct.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+
+    for (let i=0; i < constructorStandings.length; i++) {
+        for (let x=0; x < driversList.length; x++) {
+            if (driversList[x][5] === constructorStandings[i].Constructor.constructorId) {
+                driversList[x].push(constructorStandings[i].position);
+                driversList[x].push(constructorStandings[i].points);
+            }
+        }
+    }
+    return driversList;
+}
+
+async function getDriversList () {
+    driverList = [];
+    const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+
+    var drivers = await response.json();
+
+    var driverStandings = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+
+    for (let i=0; i < driverStandings.length; i++) {
+        if (driverStandings[i].Driver.code !== "HUL") {
+            driverList.push([driverStandings[i].Driver.driverId, driverStandings[i].Driver.givenName, driverStandings[i].Driver.familyName]);
+        }
+    }
+    return driverList;
+}
+
+async function predictRace () {
+
+    let driversList = await getDriversList();
+    
+    driversList = await getDriverStandings(driversList);
+    console.log(driversList);
+
+    driversList = await getConstructorStandings(driversList);
+    console.log(driversList);
+
+    //const allDriverStandings = getAllDriverStandings();
+    //const allConstructorStandings = getAllConstructorStandings();
+
+    const seasonResults = getSeasonResults();
+    const previousTrackResults = getPreviousRaces ();
+
+    const crashes = getSeasonCrashes();
+    const allCrashes = getAllCrashes();
+
+    const weather = getWeather();
+}
+
+
+//format = [driver ID, first name, last name, driver standing pos, driver points, constructor id, constructor standing pos, constructor points]
