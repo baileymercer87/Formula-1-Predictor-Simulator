ac5655893c0b0d08a9930a16fd4e4783354f9815
Tadpole87 <bailey_mercer@hotmail.co.uk>
2022-05-09 13:56:42 +0100

    Added Basic leaderboard with team colours + another fetch function for setup
    Added:
    - Fetch function to get constructors and drivers
    - A list to display drivers in future
    - Team colours hex array to use in styling

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
-async function get1() { 
-
-    const response = await fetch('http://ergast.com/api/f1/2022/drivers.json'); 
-
-
-    var data = await response.json();
-
-    const driverArray = data.MRData.DriverTable.Drivers;
-
-    for (let i=0; i< driverArray.length; i++) {
-        const para = document.createElement("p");
-        para.innerHTML = driverArray[i].driverId;
-        document.getElementById("main").appendChild(para);
-    }
-}
+const teamColors = [['ferrari', '#A6051A'], ['red_bull', '#0600EF'], ['mercedes', '	#00D2BE'], ['mclaren', '#FF8700'], ['alpine', '#0090FF'], ['alfa', '#900000'], ['haas', '	#FFFFFF'], ['alphatauri', '	#2B4562'], ['williams', '#005AFF'], ['aston_martin', '	#006F62']];
 
 async function get2() {
-    const response = await fetch('http://ergast.com/api/f1/current.json'); 
+    const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+    var drivers = await response.json();
 
-    var data = await response.json();
+    var driverStandings = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    const list = document.getElementById("presentList").getElementsByTagName("li");
 
-    const racesArray = data.MRData.RaceTable.Races;
-    console.log(data.MRData.RaceTable.Races);
-    for (let i=0; i< racesArray.length; i++) {
-        const para = document.createElement("p");
-        para.innerHTML = racesArray[i].raceName;
-        document.getElementById("main").appendChild(para);
+    for (let i=0; i < 10; i++) {
+        const lname = driverStandings[i].Driver.familyName;
+        const fname = driverStandings[i].Driver.givenName;
+        const constructor = driverStandings[i].Constructors[0].constructorId;
+        var color = getConstructor(constructor);
+        console.log(color);
+        var name = (i+1) + ". " + fname + " " + lname;
+        list[i].innerHTML = name;
+        list[i].style.backgroundColor = color;
     }
 }
 
-async function get3() {
-    const response = await fetch('http://ergast.com/api/f1/2022/4/results.json'); 
 
-    var data = await response.json();
+function getConstructor (constructor) {
+    console.log(constructor);
+    for (let i=0; i < 10; i++) {
+        if (teamColors[i][0] === constructor) {
+            return teamColors[i][1];
+        }
+    }
+}
 
-    console.log(data);
-     
+
+
+
+
+
+
+
+
+async function getCurrentStandings () {
+    const response = await fetch('http://ergast.com/api/f1/current/constructorStandings.json');
+    const response1 = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+
+    var construct = await response.json();
+    var drivers = await response1.json();
+
+    var constructorStandings = construct.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    var driverStandings = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+
+    console.log(driverStandings);
+
+    for (let i=0; i < constructorStandings.length; i++) {
+        console.log(constructorStandings[i].Constructor.name);
+    }
+
+    for (let i=0; i < driverStandings.length; i++) {
+        const lname = driverStandings[i].Driver.familyName;
+        const fname = driverStandings[i].Driver.givenName;
+        const para = document.createElement("p");
+        para.innerHTML = (i+1) + ". " + fname + " " + lname;
+        document.getElementById("main").appendChild(para);
+    }
+
 }
